9306dc5ae2c08d8a4aa0293dfbb419dec64365d6
albqvictor1508 <albq.victor@gmail.com>
2025-02-09 10:10:17 -0300

    add better practics with biome, remove eslint file and learning about swiper

--- src/test-swiper.jsx
+++ src/test-swiper.jsx
@@ -1,20 +1,23 @@
-import Swiper from "swiper/react"
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export function TestSwiper() {
-    const data = [
-        {id: 1, content: "toma no cu 1"}
-        {id: 2, content: "toma no cu 1"}
-        {id: 3, content: "toma no cu 1"}
-        {id: 4, content: "toma no cu 1"}
-        {id: 4, content: "toma no cu 1"}
-    ]
+	const data = [
+		{ id: 1, content: "toma no cu 1" },
+		{ id: 2, content: "toma no cu 2" },
+		{ id: 3, content: "toma no cu 3" },
+		{ id: 4, content: "toma no cu 4" },
+		{ id: 4, content: "toma no cu 5" },
+	];
 
-    return (
-        <div>
-                <Swiper>
-        {data.map}
-    </Swiper>
-        </div>
-    )
-
+	return (
+		<div>
+			<Swiper>
+				{data.map((lexsaLinda) => (
+					<SwiperSlide key={lexsaLinda.id}>
+						<h1>{lexsaLinda.content}</h1>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
 }
